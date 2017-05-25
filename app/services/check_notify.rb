3218b2607243cdd32aead889a1f710d924099308
Vincent <sheep0406@hotmail.com>
2017-05-25 12:08:19 +0800

    Setup for check notify

--- app/services/check_notify.rb
+++ app/services/check_notify.rb
@@ -4,22 +4,23 @@ NUM_THREADS = 4
 Thread.abort_on_exception = true
 
 def check
-  threads = []
   # 找出每筆吃藥時間的最新一筆
   query = "select r.* from record r INNER JOIN (select notification_id, max(created_at) as t from record group by notification_id) rr on r.notification_id = rr.notification_id and r.created_at = rr.created_at"
   sent_record = ActiveRecord::Base.connection.execute(query)
   # 找出今日尚未吃藥
   notifies = Notification.where("wday = ? and taken_medicine = false", "#{Date.today.strftime('%A')}")
 
-  threads << Thread.start do
-    notifies.each do |notify|
-      record = sent_record.select{ |r| r.notification_id == notify.id }
-      # 沒有紀錄 有紀錄但是日期不同
-      if !record || record.created_at.strftime('%A') != notify.wday
-        push_notifications(notify)
-        notify.update(taken_medicine: true)
+  threads = Array.new(NUM_THREADS) do
+    Thread.new do
+      notifies.each do |notify|
+        record = sent_record.select{ |r| r.notification_id == notify.id }
+        # 沒有紀錄 or 有紀錄但是日期不同
+        if !record || record.created_at.strftime('%A') != notify.wday
+          push_notifications(notify)
+          notify.update(taken_medicine: true)
+        end
       end
-    end
+    end # Thread
   end # threads
   threads.each(&:join)
 end
